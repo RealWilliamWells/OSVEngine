9a16143f5faeab0866fede801110631e880bd794
William Wells <williamwells2013@hotmail.com>
2023-01-19 00:43:52 -0500

    Setup shaders and GPU buffer for drawing example triangle shape

--- include/WIZ/rendering/RenderEngine.h
+++ include/WIZ/rendering/RenderEngine.h
@@ -5,17 +5,44 @@
 #ifndef WIZENGINE3D_RENDERENGINE_H
 #define WIZENGINE3D_RENDERENGINE_H
 
+#include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
 class RenderEngine {
     GLFWwindow* window;
+    unsigned int VBO;
+
+    // Example shader
+    const char *vertexShaderSource = "#version 330 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}\0";
+
+    const char *fragmentShaderSource = "#version 330 core\n"
+                                       "out vec4 FragColor;\n"
+                                       "\n"
+                                       "void main()\n"
+                                       "{\n"
+                                       "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "}\0";
 
 public:
     void initWindow();
 
     void openWindow();
 
+    void allocateBuffer();
+
+    void compileShaders();
+
+
     bool update();
+
+    void processInput();
+
+    void renderScreen();
 
     static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
         glViewport(0, 0, width, height);
--- src/WIZ/rendering/RenderEngine.cpp
+++ src/WIZ/rendering/RenderEngine.cpp
@@ -3,11 +3,14 @@
 #include <iostream>
 
 void RenderEngine::initWindow() {
+    glewInit();
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+//    glGenBuffers(1, &VBO);
 }
 
 void RenderEngine::openWindow() {
@@ -20,12 +23,66 @@ void RenderEngine::openWindow() {
     }
     glfwMakeContextCurrent(window);
 
+//    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to create GLEW" << std::endl;
+        glfwTerminate();
+        // TODO: throw glew init exception
+    }
+
     glViewport(0, 0, 800, 600);
 
     glfwSetFramebufferSizeCallback(window, RenderEngine::framebufferSizeCallback);
 }
 
+void RenderEngine::allocateBuffer() {
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+    };
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+void RenderEngine::compileShaders() {
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    success = 1;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
 bool RenderEngine::update() {
+    processInput();
+
+    renderScreen();
+
     glfwSwapBuffers(window);
     glfwPollEvents();
     if (glfwWindowShouldClose(window)){
@@ -33,6 +90,18 @@ bool RenderEngine::update() {
         return false;
     }
     return true;
+}
+
+void RenderEngine::processInput() {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void RenderEngine::renderScreen() {
+    // TODO: Implement screen class for rendering engine to display
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 int main() {
