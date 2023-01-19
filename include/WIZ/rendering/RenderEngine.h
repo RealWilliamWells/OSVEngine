72d7ffbba361a79bb3c9ec0da80cd75f6a39efee
William Wells <williamwells2013@hotmail.com>
2023-01-18 19:41:09 -0500

    Starting basis for rendering engine

---
+++ include/WIZ/rendering/RenderEngine.h
@@ -0,0 +1,25 @@
+//
+// Created by william on 18/01/23.
+//
+
+#ifndef WIZENGINE3D_RENDERENGINE_H
+#define WIZENGINE3D_RENDERENGINE_H
+
+#include "GLFW/glfw3.h"
+
+class RenderEngine {
+    GLFWwindow* window;
+
+public:
+    void initWindow();
+
+    void openWindow();
+
+    bool update();
+
+    static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+};
+
+#endif //WIZENGINE3D_RENDERENGINE_H
